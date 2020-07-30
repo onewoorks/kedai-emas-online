04815512bc5ef5972c2912cbad38521f7fe6d8c4
irwan ibrahim <irwanbibrahim@onewoorks.com>
2020-07-30 10:08:53 +0800

    Update index.js
    square image

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <div className="container">
         <div className="d-flex justify-content-center">
-          <img style={{width: 200}}
+          <img style={{width: 200, height:200}}
           src="https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/60890947_2590796167617113_90393092892393472_n.jpg?_nc_cat=105&_nc_sid=85a577&_nc_ohc=6b7ckHoGeK4AX9r65cf&_nc_ht=scontent-sin6-2.xx&oh=ac132fcff13cb03e68dbbfb109414546&oe=5F489E60" />
         </div>
 
